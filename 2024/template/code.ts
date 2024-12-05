b0c302c75e482ae559050ba36cf4bacb786dbf26
Austin <austinborn212@gmail.com>
2024-12-04 18:27:40 -0800

    Update template

--- 2024/template/code.ts
+++ 2024/template/code.ts
@@ -1,25 +1,32 @@
-
-import { createHash } from 'crypto'
 import { readFileSync } from 'fs'
-import { getSum, getMaxes } from '../../utils'
+
+const solvePart1 = (header: string, input) => {
+  let answer = 0
+
+  // TODO
+
+  console.log(header, answer)
+}
+
+const solvePart2 = (header: string, input) => {
+  let answer = 0
+
+  // TODO
+
+  console.log(header, answer)
+}
 
 const main = async () => {
   const input = readFileSync(`./${process.argv[2]}/day${process.argv[3]}/input.txt`, 'utf8')
+  const inputExample = readFileSync(`./${process.argv[2]}/day${process.argv[3]}/inputExample.txt`, 'utf8')
 
   console.log("Solving Puzzle:")
 
-  let answer
+  solvePart1('\nPart 1 (example): ', inputExample)
+  solvePart1('\nPart 1: ', input)
 
-
-
-  console.log('\nPart 1: ' + answer)
-
-  answer = undefined
-
-  
-
-  console.log('\nPart 2: ' + answer)
-
+  solvePart2('\nPart 2 (example): ', inputExample)
+  solvePart2('\nPart 2: ', input)
 }
 
 main()
