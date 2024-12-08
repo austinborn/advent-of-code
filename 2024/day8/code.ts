dd3ab617f4cd98573c5a7bec49e5330d1fb0a942
Austin <austinborn212@gmail.com>
2024-12-07 21:32:08 -0800

    2024 day 8 (original)

--- 2024/day8/code.ts
+++ 2024/day8/code.ts
@@ -1,19 +1,126 @@
 import { fetchExample, fetchInput, withTime } from '../../utils'
 
 const solvePart1 = (rows: string[]) => {
-  let answer = 0
+  let uniqueAntennas = new Set<string>()
+  let antennaList: Record<string, { x: number, y: number }[]> = {}
 
-  // TODO
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i]
+    for (let j = 0; j < row.length; j++) {
+      const char = row[j]
+      if (char == '.') continue
+      if (!uniqueAntennas.has(char)) {
+        uniqueAntennas.add(char)
+        antennaList[char] = [{ x: j, y: i }]
+      } else {
+        antennaList[char].push({ x: j, y: i })
+      }
+    }
+  }
 
-  return answer
+  let uniqueAntinodes = new Set()
+
+  for (let char of uniqueAntennas) {
+    let locales = antennaList[char]
+    let [thisLocale, ...remainingLocales] = locales
+    while(remainingLocales.length > 0) {
+      console.log(thisLocale, remainingLocales)
+      for (let locale of remainingLocales) {
+        let antinodeX = 2 * thisLocale.x - locale.x
+        let antinodeY = 2 * thisLocale.y - locale.y
+        if (
+          antinodeX >= 0 &&
+          antinodeX < rows[0].length &&
+          antinodeY >= 0 &&
+          antinodeY < rows.length
+        ) {
+          if (!uniqueAntinodes.has(`${antinodeX},${antinodeY}`)) uniqueAntinodes.add(`${antinodeX},${antinodeY}`)
+        }
+
+        antinodeX = 2 * locale.x - thisLocale.x
+        antinodeY = 2 * locale.y - thisLocale.y
+        if (
+          antinodeX >= 0 &&
+          antinodeX < rows[0].length &&
+          antinodeY >= 0&&
+          antinodeY < rows.length
+        ) {
+          if (!uniqueAntinodes.has(`${antinodeX},${antinodeY}`)) uniqueAntinodes.add(`${antinodeX},${antinodeY}`)
+        }
+      }
+      [thisLocale, ...remainingLocales] = remainingLocales
+    }
+  }
+
+  return uniqueAntinodes.size
 }
 
 const solvePart2 = (rows: string[]) => {
-  let answer = 0
+  let uniqueAntennas = new Set<string>()
+  let antennaList: Record<string, { x: number, y: number }[]> = {}
 
-  // TODO
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i]
+    for (let j = 0; j < row.length; j++) {
+      const char = row[j]
+      if (char == '.') continue
+      if (!uniqueAntennas.has(char)) {
+        uniqueAntennas.add(char)
+        antennaList[char] = [{ x: j, y: i }]
+      } else {
+        antennaList[char].push({ x: j, y: i })
+      }
+    }
+  }
 
-  return answer
+  let uniqueAntinodes = new Set()
+
+  for (let char of uniqueAntennas) {
+    let locales = antennaList[char]
+    let [thisLocale, ...remainingLocales] = locales
+    while(remainingLocales.length > 0) {
+      for (let locale of remainingLocales) {
+        const diffx = thisLocale.x - locale.x
+        const diffy = thisLocale.y - locale.y
+
+        let upperx = (
+          diffx > 0
+            ? ((thisLocale.x > locale.x) ? thisLocale.x : locale.x)
+            : ((thisLocale.x < locale.x) ? thisLocale.x : locale.x)
+          ) + diffx
+        let uppery = (
+          diffx > 0
+            ? ((thisLocale.x > locale.x) ? thisLocale.y : locale.y)
+            : ((thisLocale.x < locale.x) ? thisLocale.y : locale.y)
+          ) + diffy
+        while ( upperx >= 0 && upperx < rows[0].length && uppery < rows.length && uppery >= 0) {
+          if (!uniqueAntinodes.has(`${upperx},${uppery}`)) uniqueAntinodes.add(`${upperx},${uppery}`)
+          upperx += diffx
+          uppery += diffy
+        }
+
+        upperx = (
+          diffx > 0
+            ? ((thisLocale.x < locale.x) ? thisLocale.x : locale.x)
+            : ((thisLocale.x > locale.x) ? thisLocale.x : locale.x)
+          ) + diffx
+        uppery = (
+          diffx > 0
+            ? ((thisLocale.x < locale.x) ? thisLocale.y : locale.y)
+            : ((thisLocale.x > locale.x) ? thisLocale.y : locale.y)
+          ) + diffy
+        while ( upperx >= 0 && upperx < rows[0].length && uppery < rows.length && uppery >= 0) {
+          if (!uniqueAntinodes.has(`${upperx},${uppery}`)) uniqueAntinodes.add(`${upperx},${uppery}`)
+          upperx -= diffx
+          uppery -= diffy
+        }
+      }
+
+      [thisLocale, ...remainingLocales] = remainingLocales
+    }
+  }
+
+  return uniqueAntinodes.size
 }
 
 const main = async () => {
