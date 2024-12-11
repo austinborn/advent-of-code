a67fea40a1f01d44d581802babb9bb6fae312ede
Austin <austinborn212@gmail.com>
2024-12-10 21:54:54 -0800

    Clean up naming

--- 2024/day11/code.ts
+++ 2024/day11/code.ts
@@ -1,27 +1,27 @@
 import { fetchExample, fetchInput, withTime } from '../../utils'
 
-const cache = {}
+const finalStoneCount = {}
 
-const numStonesForBlinks = (stones: number[], blinks: number) => {
-  if (blinks == 0) return stones.length
-  if (!cache[blinks]) cache[blinks] = {}
+const getNumStones = (stones: number[], blinksLeft: number) => {
+  if (blinksLeft == 0) return stones.length
+  if (!finalStoneCount[blinksLeft]) finalStoneCount[blinksLeft] = {}
 
   let total = 0
-  for (let oldStone of stones) {
-    if (cache[blinks][oldStone]) total += cache[blinks][oldStone]
+  for (let stone of stones) {
+    if (finalStoneCount[blinksLeft][stone]) total += finalStoneCount[blinksLeft][stone]
     else {
       let num
-      if (oldStone == 0) {
-        num = numStonesForBlinks([1], blinks - 1)
-      } else if (oldStone.toString().length % 2 == 0) {
-        const string = oldStone.toString()
+      if (stone == 0) {
+        num = getNumStones([1], blinksLeft - 1)
+      } else if (stone.toString().length % 2 == 0) {
+        const string = stone.toString()
         const left = parseInt(string.slice(0, string.length / 2))
         const right = parseInt(string.slice(string.length / 2))
-        num = numStonesForBlinks([left, right], blinks - 1)
+        num = getNumStones([left, right], blinksLeft - 1)
       } else {
-        num = numStonesForBlinks([oldStone * 2024], blinks - 1)
+        num = getNumStones([stone * 2024], blinksLeft - 1)
       }
-      cache[blinks][oldStone] = num
+      finalStoneCount[blinksLeft][stone] = num
       total += num
     }
   }
@@ -29,24 +29,19 @@ const numStonesForBlinks = (stones: number[], blinks: number) => {
   return total
 }
 
-const solvePart1 = (input: string[], blinkCount: number) => {
+const solve = (input: string[], blinkCount: number) => {
   let stones = input[0].split(' ').map(c => parseInt(c))
-  return numStonesForBlinks(stones, blinkCount)
-}
-
-const solvePart2 = (input: string[], blinkCount: number) => {
-  let stones = input[0].split(' ').map(c => parseInt(c))
-  return numStonesForBlinks(stones, blinkCount)
+  return getNumStones(stones, blinkCount)
 }
 
 const main = async () => {
   const input = (await fetchInput()).split('\n')
   const inputExample = (await fetchExample()).split('\n')
 
-  console.log('\nPart 1 (example):', withTime(() => solvePart1(inputExample, 25)))
-  console.log('\nPart 1:', withTime(() => solvePart1(input, 25)))
-  console.log('\nPart 1:', withTime(() => solvePart2(inputExample, 75)))
-  console.log('\nPart 2:', withTime(() => solvePart2(input, 75)))
+  console.log('\nPart 1 (example):', withTime(() => solve(inputExample, 25)))
+  console.log('\nPart 1:', withTime(() => solve(input, 25)))
+  console.log('\nPart 1:', withTime(() => solve(inputExample, 75)))
+  console.log('\nPart 2:', withTime(() => solve(input, 75)))
 }
 
 main()
